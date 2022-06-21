b19e58dc3c34b7fdcfe79e552d504667bcbe8331
ThomasQtl <thomas.quentel2@gmail.com>
2022-06-21 11:12:45 +0200

    Data Ok

---
+++ JeuxDeLaVie/data.h
@@ -0,0 +1,10 @@
+#ifndef __DATA_H__
+#define __DATA_H__
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+int naissance[9] = {0, 1, 1, 0, 0, 0, 0, 0, 0};
+int survie[9] = {0, 0, 0, 1, 1, 1, 1, 1, 1};
+
+#endif
