5e1147d3ab7ca9d75e31aca867ca932c81216037
ThomasQtl <thomas.quentel2@gmail.com>
2022-06-21 13:37:41 +0200

    Ajout de la taille dans le fichier

--- JeuxDeLaVie/save.c
+++ JeuxDeLaVie/save.c
@@ -1,15 +1,20 @@
 #include "save.h"
 
 void chargerConfig(char *nom_fichier, int **config, int taille_X, int taille_Y)
-{
+{ // On passe taille_X et taille_Y en passage par adresse pour les récupérer.
+  // Le fichier contient sur la 1ère ligne: taille_X taille_Y
+  // Puis: 11100011011\n
+  //       11001101100\n
     FILE *file = fopen(nom_fichier, "r");
     if (file)
     {
         int i, j;
-        for (i=0; i<taille_X; i++) {
-            fgets(ligne, taille_Y, file);
-            for (j=0; j<taille_Y; j++) {
-                config[i][j] = atoi(ligne[j]);
+        fscanf(file, "%i %i", taille_X, taille_Y);
+        for (i = 0; i < taille_X; i++)
+        {
+            for (j = 0; j < taille_Y; j++)
+            {
+                fscanf(file, "%d", config[i][j]);
             }
         }
     }
@@ -18,12 +23,15 @@ void chargerConfig(char *nom_fichier, int **config, int taille_X, int taille_Y)
 
 void saveConfig(char *nom_fichier, int **config, int taille_X, int taille_Y)
 {
-    FILE *file = fopen(nom_fic, "w");
+    FILE *file = fopen(nom_fichier, "w");
     if (file)
     {
+        fprintf(file, "%i %i\n", taille_X, taille_Y);
         int i, j;
-        for (i=0; i<taille_X; i++) {
-            for (j=0; j<taille_Y; j++) {
+        for (i = 0; i < taille_X; i++)
+        {
+            for (j = 0; j < taille_Y; j++)
+            {
                 fprintf(file, "%i", config[i][j]);
             }
             fprintf(file, "\n");
