778cf4e80db9d7aef86304e22544254d76236e8b
ivleteuff <ivleteuff@etu.uca.com>
2022-06-21 12:01:37 +0200

    avancement cycle.c (pause midi)

--- JeuxDeLaVie/cycle.h
+++ JeuxDeLaVie/cycle.h
@@ -1,9 +1,10 @@
 #ifndef __CYCLE_H__
 #define __CYCLE_H__
 #include <stdio.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 
 /* Déclaration des prototypes */
+
+int nbVoisin(int i, int j,int **cour);
+void cycle(int** nouv, int** anc);
 
 #endif
