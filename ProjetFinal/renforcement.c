7746bbf6f0ce4f234a212d82f9867b00c5b4a804
Thomas Quentel <thomas.quentel@etu.uca.fr>
2022-06-27 11:25:17 +0200

    Struct joueur

--- ProjetFinal/renforcement.c
+++ ProjetFinal/renforcement.c
@@ -1,25 +1,20 @@
-#include "renforcement.h"
+#ifndef __RENFORCEMENT_H__
+#define __RENFORCEMENT_H__
 
-void perception()
-{ // Prend l'état du monde et renvoie l'état de perception s
-}
+typedef struct joueur
+{
+    int x;
+    int y;
+    enum ETATS qtable[NBETATS][NBETATS];
+} joueur_t;
 
-void evolution()
-{ // Prend le monde et une action et modifie l'état du monde
-}
+/* Déclaration des prototypes */
 
-void recompense()
-{ // Prend le monde et renvoie la récompense associé à cet état
-}
+void perception();         // Prend l'état du monde et renvoie l'état de perception s
+void evolution();          // Prend le monde et une action et modifie l'état du monde
+void recompense();         // Prend le monde et renvoie la récompense associé à cet état
+void enumerationActions(); // Prend le monde et renvoie une énumération des actions possibles
+void choixAction();        // Prend une perception et le monde et renvoie l'action choisie
+void renforcement();       // Fonction principale
 
-void enumerationActions()
-{ // Prend le monde et renvoie une énumération des actions possibles
-}
-
-void choixAction()
-{ // Prend une perception et le monde et renvoie l'action choisie
-}
-
-void renforcement()
-{ // Fonction principale
-}
+#endif
