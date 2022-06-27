58aca8e6b5dc2e41f526a40daff9488172555cc4
Thomas Quentel <thomas.quentel@etu.uca.fr>
2022-06-27 10:46:48 +0200

    Ajout prototypes fonctions renforcement

---
+++ ProjetFinal/renforcement.c
@@ -0,0 +1,25 @@
+#include "renforcement.h"
+
+void perception()
+{ // Prend l'état du monde et renvoie l'état de perception s
+}
+
+void evolution()
+{ // Prend le monde et une action et modifie l'état du monde
+}
+
+void recompense()
+{ // Prend le monde et renvoie la récompense associé à cet état
+}
+
+void enumerationActions()
+{ // Prend le monde et renvoie une énumération des actions possibles
+}
+
+void choixAction()
+{ // Prend une perception et le monde et renvoie l'action choisie
+}
+
+void renforcement()
+{ // Fonction principale
+}
