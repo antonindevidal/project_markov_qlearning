592e6f65ba33499c32faf9b13493e5b2b66c23a4
Thomas Quentel <thomas.quentel@etu.uca.fr>
2022-06-27 11:50:50 +0200

    choixAction() ok

--- ProjetFinal/renforcement.c
+++ ProjetFinal/renforcement.c
@@ -1,23 +1,17 @@
-#ifndef __RENFORCEMENT_H__
-#define __RENFORCEMENT_H__
+#include "renforcement.h"
 
-typedef struct joueur
-{
-    int x;
-    int y;
-    enum ETATS qtable[NBETATS][NBETATS];
-} joueur_t;
+void perception()
+{ // Prend l'état du monde et renvoie l'état de perception s
+}
 
-/* Déclaration des prototypes */
+void evolution()
+{ // Prend le monde et une action et modifie l'état du monde
+}
 
-void perception();         // Prend l'état du monde et renvoie l'état de perception s
-void evolution();          // Prend le monde et une action et modifie l'état du monde
-void recompense();         // Prend le monde et renvoie la récompense associé à cet état
-void enumerationActions(); // Prend le monde et renvoie une énumération des actions possibles
-void choixAction();        // Prend une perception et le monde et renvoie l'action choisie
-void renforcement();       // Fonction principale
+void recompense()
+{ // Prend le monde et renvoie la récompense associé à cet état
+}
 
-<<<<<<< HEAD
 void enumerationActions()
 { // Prend le monde et renvoie une énumération des actions possibles
 }
@@ -43,6 +37,4 @@ int choixAction(ordinateur_t *ordi, int perception)
 void renforcement()
 { // Fonction principale
 }
-=======
-#endif
->>>>>>> dd635d6 (Struct joueur)
+
