88cec077a99d1b53fe403f24ef977146739c8c96
Thomas Quentel <thomas.quentel@etu.uca.fr>
2022-06-27 15:54:53 +0200

    Evolution() ok

--- ProjetFinal/renforcement/renforcement.c
+++ ProjetFinal/renforcement/renforcement.c
@@ -4,8 +4,21 @@ void perception()
 { // Prend l'état du monde et renvoie l'état de perception s
 }
 
-void evolution()
+void evolution(ordinateur_t *ordi, int *suiteEtats, int *suiteActions, int *suiteRecompenses, int n)
 { // Prend le monde et une action et modifie l'état du monde
+    int i, max, a;
+    ordi->QTable[suiteEtats[n-1]][suiteActions[n-1]] += XI \
+    * (suiteActions[n] - ordi->QTable[suiteActions[n-1]][suiteEtats[n-1]]);
+    for (i=n-2; i>0; i--) {
+        max = ordi->QTable[suiteEtats[i+1]][0];
+        for (a=1; a<NBACTIONS; a++) {
+            if (max < ordi->QTable[i+1][a]) {
+                max = ordi->QTable[i+1][a];
+            }
+            ordi->QTable[suiteEtats[i]][suiteActions[i]] += XI * (suiteRecompenses[i+1]  \
+            + GAMMA * max - ordi->QTable[suiteEtats[i]][suiteActions[i]]);
+        }
+    }
 }
 
 void recompense()
@@ -16,7 +29,7 @@ void enumerationActions()
 { // Prend le monde et renvoie une énumération des actions possibles
 }
 
-int choixAction(ordinateur_t *ordi, int s)
+int choixAction(ordinateur_t *ordi, int s, int T)
 { // Prend une perception et le monde et renvoie l'action choisie
     int a=0; // Parcours de actions
     int action = NBACTIONS-1; // Action par défaut
@@ -25,11 +38,12 @@ int choixAction(ordinateur_t *ordi, int s)
     float alpha; // Réel aléatoire dans [0; 1[
     int cumul = 0;
     for (a=0; a<NBACTIONS; a++) {
-        E[a] = exp(ordi->QTable[s][a]);
+        E[a] = exp(ordi->QTable[s][a]/T);
         Z += E[a];
     }
     alpha = rand();
     for (a=0; a<NBACTIONS; a++) {
+        cumul += E[a]/Z;
         if (alpha <= cumul) {
             action = a;
             break;
@@ -40,4 +54,5 @@ int choixAction(ordinateur_t *ordi, int s)
 
 void renforcement()
 { // Fonction principale
+
 }
--- ProjetFinal/renforcement/renforcement.h
+++ ProjetFinal/renforcement/renforcement.h
@@ -14,10 +14,10 @@ typedef struct ordinateur
 /* Déclaration des prototypes */
 
 void perception();         // Prend l'état du monde et renvoie l'état de perception s
-void evolution();          // Prend le monde et une action et modifie l'état du monde
+void evolution(ordinateur_t *ordi, int *suiteEtats, int *suiteActions, int *suiteRecompenses, int n);
 void recompense();         // Prend le monde et renvoie la récompense associé à cet état
 void enumerationActions(); // Prend le monde et renvoie une énumération des actions possibles
-int choixAction(ordinateur_t *ordi, int s);        // Prend une perception et le monde et renvoie l'action choisie
+int choixAction(ordinateur_t *ordi, int s, int T);
 void renforcement();       // Fonction principale
 
 #endif
