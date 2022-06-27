301e8903b672dcb4083ceaeded8403a04c5744fb
Thomas Quentel <thomas.quentel@etu.uca.fr>
2022-06-27 17:06:06 +0200

    initQTableOrdi() ok

--- ProjetFinal/player/player.c
+++ ProjetFinal/player/player.c
@@ -33,3 +33,12 @@ void movePlayer(player_t *p, enum DIRECTION dir)
 void playerBallCollision(player_t *p, ball_t *b)
 {
 }
+
+void initQTableOrdi(ordinateur_t *ordi) {
+    int i, j;
+    for (i=0; i<NBETATS; i++) {
+        for (j=0; j<NBACTIONS; j++) {
+            ordi->QTable[i][j] = 0;
+        }
+    }
+}
